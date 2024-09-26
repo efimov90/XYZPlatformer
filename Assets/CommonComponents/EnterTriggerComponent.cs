71b20da8f11a502e43453aa0408847954c8f2ec4
Sergey Efimov <efimov90@gmail.com>
2024-09-26 03:03:54 +0300

    Камера, уровень

---
+++ Assets/CommonComponents/EnterTriggerComponent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.CommonComponents
+{
+    public class EnterTriggerComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private string _tag;
+
+        [SerializeField]
+        private UnityEvent _action;
+
+        private void OnTriggerEnter2D(Collider2D otherCollider)
+        {
+            if (otherCollider.CompareTag(_tag))
+            {
+                _action?.Invoke();
+            }
+        }
+    }
+}
