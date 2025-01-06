518faf4c6a49c887189755f41727d27f2932a543
Sergey Efimov <efimov90@gmail.com>
2025-01-06 19:56:57 +0300

    CheatsController

---
+++ Assets/CommonComponents/Cheat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.CommonComponents
+{
+    [Serializable]
+    public class Cheat
+    {
+        [SerializeField]
+        public string CheatCode;
+        [SerializeField]
+        public UnityEvent Action;
+    }
+}
---
+++ Assets/CommonComponents/CheatController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.CommonComponents
+{
+    public class CheatController : MonoBehaviour
+    {
+        private string _currentInput;
+        
+        [SerializeField] private float _inputTimeToLive;
+        [SerializeField] private Cheat[] _cheats;
+
+        private float _inputTime;
+
+        private void Awake()
+        {
+            Keyboard.current.onTextInput += OnTextInput;
+        }
+
+        private void Update()
+        {
+            if (_inputTime > 0)
+            {
+                _inputTime -= Time.deltaTime;
+            }
+            else
+            {
+                _currentInput = string.Empty;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            Keyboard.current.onTextInput -= OnTextInput;
+        }
+
+        private void OnTextInput(char inputSymbol)
+        {
+            _currentInput += inputSymbol;
+            _inputTime = _inputTimeToLive;
+            FindAndExecuteCheatIfExists();
+        }
+
+        private void FindAndExecuteCheatIfExists()
+        {
+            foreach (var cheat in _cheats)
+            {
+                if (_currentInput.Contains(cheat.CheatCode))
+                {
+                    cheat.Action?.Invoke();
+                    _currentInput = string.Empty;
+
+                    return;
+                }
+            }
+        }
+    }
+}
