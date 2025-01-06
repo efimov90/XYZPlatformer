cda4e06789ee09b0d2b146c0775ed6b482dcd9eb
Sergey Efimov <efimov90@gmail.com>
2025-01-07 00:17:09 +0300

    Компонент

---
+++ Assets/CommonComponents/InteractableComponent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.CommonComponents
+{
+    public class InteractableComponent : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent _action;
+
+        private void Interact()
+        {
+            _action?.Invoke();
+        }
+    }
+}
