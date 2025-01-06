a26544316488f688002c6fae8890db83951d19ce
Sergey Efimov <efimov90@gmail.com>
2025-01-06 16:42:07 +0300

    Правки из уроков
    Фикс залипания
    Система анимации

--- Assets/PixelCrew/Hero.cs
+++ Assets/PixelCrew/Hero.cs
@@ -13,10 +13,12 @@ public class Hero : MonoBehaviour
 
     private Rigidbody2D _rigidbody2D;
     private Vector2 _direction = Vector2.zero;
+    private Animator _animator;
 
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     public void SetDirection(Vector2 direction)
@@ -35,9 +37,11 @@ public class Hero : MonoBehaviour
 
         var isJumping = _direction.y > 0;
 
+        var isOnFloor = IsOnFloor();
+
         if (isJumping)
         {
-            if (IsOnFloor() && _rigidbody2D.velocity.y <= 0)
+            if (isOnFloor && _rigidbody2D.velocity.y <= 0)
             {
                 _rigidbody2D.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
             }
@@ -46,6 +50,10 @@ public class Hero : MonoBehaviour
         {
             _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _rigidbody2D.velocity.y * 0.5f);
         }
+
+        _animator.SetBool("IsOnFloor", isOnFloor);
+        _animator.SetFloat("VerticalVelocity", _rigidbody2D.velocity.y);
+        _animator.SetBool("IsRunning", _direction.x != 0);
     }
 
     private bool IsOnFloor() => _groundCollisionCheck.IsTouchingLayer;
