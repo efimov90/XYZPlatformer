13df1b14bca232d6866c4b313d5b6027d4887b9e
Sergey Efimov <efimov90@gmail.com>
2025-02-13 23:32:17 +0300

    ExitLevel

---
+++ Assets/CommonComponents/ExitLevelComponent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.CommonComponents
+{
+    public class ExitLevelComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private string _sceneName;
+
+        public void Exit()
+        {
+            Debug.Log($"Exit triggered");
+            SceneManager.LoadScene(_sceneName);
+        }
+    }
+}
