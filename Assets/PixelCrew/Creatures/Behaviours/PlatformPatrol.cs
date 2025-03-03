b963f8474ac6b23a9f999177fb81a1b0ea657605
Sergey Efimov <efimov90@gmail.com>
2025-03-03 22:38:32 +0300

    Патруль платформы

--- Assets/PixelCrew/Creatures/Behaviours/PlatformPatrol.cs
+++ Assets/PixelCrew/Creatures/Behaviours/PlatformPatrol.cs
@@ -1,13 +1,38 @@
-﻿using System;
+﻿using Assets.CommonComponents;
 using System.Collections;
+using UnityEngine;
 
 namespace Assets.PixelCrew.Creatures.Behaviours
 {
     public class PlatformPatrol : Patrol
     {
+        [SerializeField]
+        private LayerCollisionCheck _groundProbe;
+
+        private Creature _creature;
+
+        private void Awake()
+        {
+            _creature = GetComponent<Creature>();
+        }
+
         public override IEnumerator DoPatrol()
         {
-            throw new NotImplementedException();
+            while (enabled)
+            {
+                if (!_groundProbe.IsTouchingLayer)
+                {
+                    var newDirection = _creature.transform.position - _groundProbe.transform.position;
+                    newDirection.y = 0;
+
+                    _creature.SetDirection(newDirection.normalized);
+
+                    // Полагаю тут что-то другое должно быть
+                    yield return new WaitForSeconds(1);
+                }
+
+                yield return null;
+            }
         }
     }
 }
