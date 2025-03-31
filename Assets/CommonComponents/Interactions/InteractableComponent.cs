d64828912ee3dbece00bd3191fbfeb947d438697
Sergey Efimov <efimov90@gmail.com>
2025-03-31 04:09:16 +0300

    InteractionComponents

--- Assets/CommonComponents/Interactions/InteractableComponent.cs
+++ Assets/CommonComponents/Interactions/InteractableComponent.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 
-namespace Assets.CommonComponents
+namespace Assets.CommonComponents.Interactions
 {
     public class InteractableComponent : MonoBehaviour
     {
--- Assets/CommonComponents/Interactions/InteractorComponent.cs
+++ Assets/CommonComponents/Interactions/InteractorComponent.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace Assets.CommonComponents
+namespace Assets.CommonComponents.Interactions
 {
     public class InteractorComponent : MonoBehaviour
     {
