40bb85a9ee5aec395fdbd52843d3fefd5098e8c4
Sergey Efimov <efimov90@gmail.com>
2025-03-31 05:22:51 +0300

    Seashell part

--- Assets/PixelCrew/Creatures/Weapons/Projectile.cs
+++ Assets/PixelCrew/Creatures/Weapons/Projectile.cs
@@ -11,12 +11,17 @@ namespace Assets.PixelCrew.Creatures.Weapons
     {
         [SerializeField]
         private float _speed;
+
+        [SerializeField]
+        private bool invertX = false;
+
         private int _direction;
         private Rigidbody2D _rigidbody;
 
         private void Start()
         {
-            _direction = transform.lossyScale.x > 0 ? 1 : -1;
+            var modifier = invertX ? -1 : 1;
+            _direction = modifier * transform.lossyScale.x > 0 ? 1 : -1;
             _rigidbody = GetComponent<Rigidbody2D>();
             var force = new Vector2(_direction * _speed, 0);
             _rigidbody.AddForce(force, ForceMode2D.Impulse);
