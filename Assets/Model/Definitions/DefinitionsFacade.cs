af6411e4df9a46a510deaf749968c7c02fae7f56
Sergey Efimov <efimov90@gmail.com>
2025-06-24 20:33:53 +0300

    Fixes

--- Assets/Model/Definitions/DefinitionsFacade.cs
+++ Assets/Model/Definitions/DefinitionsFacade.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Assets.Model.Definitions
 {
@@ -19,7 +14,7 @@ namespace Assets.Model.Definitions
             => _inventoryItemDefinitions;
 
         public static DefinitionsFacade Instance
-            => _instance ? LoadDefinitions() : _instance;
+            => _instance == null ? LoadDefinitions() : _instance;
 
         private static DefinitionsFacade LoadDefinitions()
             => _instance = Resources.Load<DefinitionsFacade>("DefinitionsFacade");
