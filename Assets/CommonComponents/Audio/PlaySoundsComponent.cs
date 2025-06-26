ead8907ad4c80b39fe6369344012df60aa538928
Sergey Efimov <efimov90@gmail.com>
2025-06-26 13:12:12 +0300

    Audio Component

---
+++ Assets/CommonComponents/Audio/PlaySoundsComponent.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Assets.CommonComponents.Audio
+{
+    public class PlaySoundsComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSource _audioSource;
+
+        [SerializeField]
+        private AudioData[] _sounds;
+
+        public void Play(string id)
+        {
+            if(_sounds.FirstOrDefault(x => x.Id == id) is AudioData audioData)
+            {
+                if (audioData.Clip != null)
+                {
+                    _audioSource.PlayOneShot(audioData.Clip);
+                }
+                else
+                {
+                    Debug.LogWarning($"Audio clip for ID '{id}' is null.");
+                }
+            }
+            else
+            {
+                Debug.LogWarning($"No audio data found for ID '{id}'.");
+            }
+        }
+    }
+}
