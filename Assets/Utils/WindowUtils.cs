55d5b537a0a2d1faee1071d289d7eed22aea09a4
Sergey Efimov <efimov90@gmail.com>
2025-07-13 08:31:34 +0300

    CrabbyPerks

---
+++ Assets/Utils/WindowUtils.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Utils
+{
+    public class WindowUtils
+    {
+        public static void CreateWindow(string path)
+        {
+            var window = Resources.Load<GameObject>(path);
+            var canvas = Object.FindObjectOfType<Canvas>();
+            Object.Instantiate(window, canvas.transform);
+        }
+    }
+}
