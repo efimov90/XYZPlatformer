d3303a79e1cea45e485517472a9d9c4d9cbdd22c
Sergey Efimov <efimov90@gmail.com>
2025-07-18 23:28:40 +0300

    BloodSplash Effect and UI Fix

---
+++ Assets/PixelCrew/CommonComponents/Effects/CameraRelated/BloodSplashOverlay.cs
@@ -0,0 +1,44 @@
+﻿using Assets.Model;
+using Assets.Model.Definitions.Player;
+using Assets.Utils.Disposables;
+using UnityEngine;
+
+namespace Assets.PixelCrew.CommonComponents.Effects.CameraRelated
+{
+    [RequireComponent(typeof(Animator))]
+    public class BloodSplashOverlay : MonoBehaviour
+    {
+        private static readonly int Health = Animator.StringToHash("Health");
+        private readonly CompositeDisposable _trash = new CompositeDisposable();
+        private GameSession _gameSession;
+        private Animator _animator;
+        private Vector3 _overScale;
+
+        [SerializeField]
+        private Transform _overlay;
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+            _overScale = _overlay.localScale - Vector3.one;
+
+            _gameSession = FindObjectOfType<GameSession>();
+            _trash.Retain(_gameSession.PlayerData.Health.SubscribeAndInvoke(OnHealthChanged));
+        }
+
+        private void OnHealthChanged(int newValue, int _)
+        {
+            var maxHealth = _gameSession.StatsModel.GetCurrentValue(StatId.Health);
+            var healthNormalized = newValue / maxHealth;
+            _animator.SetFloat(Health, healthNormalized);
+
+            var overlayModifier = Mathf.Max(healthNormalized - 0.3f, 0f);
+            _overlay.localScale = Vector3.one + _overScale * overlayModifier;
+        }
+
+        private void OnDestroy()
+        {
+            _trash.Dispose();
+        }
+    }
+}
--- Assets/Utils/WindowUtils.cs
+++ Assets/Utils/WindowUtils.cs
@@ -7,7 +7,7 @@ namespace Assets.Utils
         public static void CreateWindow(string path)
         {
             var window = Resources.Load<GameObject>(path);
-            var canvas = Object.FindObjectOfType<Canvas>();
+            var canvas = GameObject.FindWithTag("MainUICanvas").GetComponent<Canvas>();
             Object.Instantiate(window, canvas.transform);
         }
     }
