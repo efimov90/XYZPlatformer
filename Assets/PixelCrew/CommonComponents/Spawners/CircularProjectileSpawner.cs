61b6375bac99bf45213f1c05ed433209f56ec3ee
Sergey Efimov <efimov90@gmail.com>
2025-07-19 20:46:40 +0300

    Ready boss

--- Assets/PixelCrew/CommonComponents/Spawners/CircularProjectileSpawner.cs
+++ Assets/PixelCrew/CommonComponents/Spawners/CircularProjectileSpawner.cs
@@ -8,13 +8,9 @@ namespace Assets.PixelCrew.CommonComponents.Spawners
     public class CircularProjectileSpawner : MonoBehaviour
     {
         [SerializeField]
-        private GameObject _projectilePrefab;
+        private CirculaprojectileSettings[] _settings;
 
-        [SerializeField]
-        private int _projectilesCount;
-
-        [SerializeField]
-        private float _delay;
+        public int Stage { get; set; }
 
         [ContextMenu("Launch Projectiles")]
         public void LaunchProjectiles()
@@ -24,18 +20,20 @@ namespace Assets.PixelCrew.CommonComponents.Spawners
 
         private IEnumerator SpawnProjectiles()
         {
-            var sectorStep = 2 * Mathf.PI / _projectilesCount;
-            for (int i = 0; i < _projectilesCount; i++)
+            var settings = _settings[Stage];
+
+            var sectorStep = 2 * Mathf.PI / settings.ProjectilesCount;
+            for (int i = 0; i < settings.ProjectilesCount; i++)
             {
                 var angle = sectorStep * i;
                 var direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
-                var instance = SpawnUtils.Spawn(_projectilePrefab, transform.position);
+                var instance = SpawnUtils.Spawn(settings.ProjectilePrefab, transform.position);
 
                 var projectile = instance.GetComponent<DirectionalProjectile>();
 
                 projectile.Launch(direction);
 
-                yield return new WaitForSeconds(_delay);
+                yield return new WaitForSeconds(settings.Delay);
             }
         }
     }
---
+++ Assets/PixelCrew/Creatures/Bosses/Behaviours/BossNextStageState.cs
@@ -0,0 +1,17 @@
+﻿using Assets.PixelCrew.CommonComponents;
+using Assets.PixelCrew.CommonComponents.Spawners;
+using UnityEngine;
+
+namespace Assets.PixelCrew.Creatures.Bosses.Behaviours
+{
+    public class BossNextStageState : StateMachineBehaviour
+    {
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            var spawner = animator.GetComponent<CircularProjectileSpawner>();
+            spawner.Stage++;
+            var changeLights = animator.GetComponent<ChangeLightsComponent>();
+            changeLights.SetColor();
+        }
+    }
+}
